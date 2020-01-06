e330af095660f9e679d24843025b0b876e8978a1
Yosuke C Sato <yosuke.c.sato@gmail.com>
2020-01-07 02:15:54 +0900

    finish creating routes and layouts

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'bootstrap-sass', '3.3.7'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 # Use Puma as the app server
@@ -56,6 +58,12 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+
+  gem 'rails-controller-testing'
+  gem 'minitest'
+  gem 'minitest-reporters'
+  gem 'guard'
+  gem 'guard-minitest'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返します
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def string_message(str = '')
+    if str.empty?
+      "It's an empty string!"
+    else
+      "The string is nonempty. (#{str})"
+    end
+  end
+
+  def palindrome_tester(s)
+    if s == s.reverse
+      "It's a palindrome!"
+    else
+      "It's not a palindrome."
+    end
+  end
+
+  def nazo
+    a = (1..10)
+    b = Range.new(1, 10)
+    a == b
+  end
+
 end
